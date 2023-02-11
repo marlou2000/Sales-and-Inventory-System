b91372ca8e737b90841f5346f0efc233191b870e
Marlou <marlou.03022000@gmail.com>
2023-02-11 17:48:44 +0800

    Updated database

--- Sales and inventory system/Table queries/Orders_history.sql
+++ Sales and inventory system/Table queries/Orders_history.sql
@@ -1,6 +1,6 @@
 CREATE TABLE orders_history(
 	history_id INT PRIMARY KEY IDENTITY(1,1),
-	item_id INT NOT NULL FOREIGN KEY REFERENCES items(item_id),
+	item_id INT NOT NULL FOREIGN KEY REFERENCES item(item_serial_number),
 	sales_id INT NOT NULL FOREIGN KEY REFERENCES sales_history(sales_id),
 	date_ordered INT NULL FOREIGN KEY REFERENCES date(date_id),
 	ordered_quantity INT NOT NULL,
--- Sales and inventory system/Table queries/items.sql
+++ Sales and inventory system/Table queries/items.sql
@@ -1,7 +1,9 @@
-CREATE TABLE items(
-	item_id INT PRIMARY KEY IDENTITY(1,1),
+CREATE TABLE item(
+	item_serial_number INT PRIMARY KEY IDENTITY(1,1),
 	item_name VARCHAR(255) NOT NULL,
 	item_price FLOAT NOT NULL,
 	item_description TEXT,
+	item_model VARCHAR(255) NOT NULL,
+	item_warranty INT NOT NULL,
 	item_added_date DATE NOT NULL
 )
